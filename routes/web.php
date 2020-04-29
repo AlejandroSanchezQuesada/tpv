2874fbda06e9a79e432c4ba733b3811774576d19
Alejandro Sánchez Quesada <48472291+7963289@users.noreply.github.com>
2020-04-29 13:46:05 +0200

    Eliminacion archivos de prueba y modificada ruta

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/probando', function () {
-    return view('probando');
-});
-
-Route::get('/probandoproductos', function () {
-    return view('probandoproductos');
-});
-
-Route::get('/login', function () {
-    return view('login');
-});
